7192c0d070d8f96aa799576f151129d7dbd1354e
shadymeowy <tolgademirdal@gmail.com>
2023-04-01 07:40:57 +0300

    change setup.py to PySide6

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
 from setuptools import setup
 
-try:
-    import PySide6
-    install_requires = ['PySide6']
-except ImportError:
-    install_requires = ['PySide2']
-
 setup(
     name='QPrimaryFlightDisplay',
     version='1.0.0a1',
     description='T-standard compliant primary flight display widget for showing realtime flight data',
     author='Tolga Demirdal',
     url='https://github.com/shadymeowy/QPrimaryFlightDisplay',
-    install_requires=install_requires,
+    install_requires=['PySide6'],
     py_modules=["QPrimaryFlightDisplay",]
 )
